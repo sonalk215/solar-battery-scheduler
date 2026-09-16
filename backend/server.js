c310abac464309fd45e3e557bf533e496ba65671
SonalKhare <sonalkhare.sk215@gmail.com>
2026-09-16 12:16:23 +1000

    backend payment

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,11 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
-// const axios = require('axios');
-
-// const { validateAssignment } = require('./services/rules');
 
 const installerRoute = require('./src/routes/installers');
 const jobsRouter = require('./src/routes/jobs');
 const weatherRouter = require('./src/routes/weather');
+const paymentRoutes = require('./src/routes/paymentRoutes');
 
 const app = express();
 app.use(cors());
@@ -17,6 +15,7 @@ app.use(express.json());
 app.use('/api/installers', installerRoute);
 app.use('/api/jobs', jobsRouter);
 app.use('/api/weather', weatherRouter);
+app.use('/api/payments', paymentRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
